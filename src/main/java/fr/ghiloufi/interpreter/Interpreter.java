f09ced1306cca0ef7afad0c3e1b5365aa4fdb87e
ghiloufi <belgacemghiloufi2021@gmail.com>
2023-05-08 19:49:56 +0200

    feat: add support for some math operation.

--- src/main/java/fr/ghiloufi/interpreter/Interpreter.java
+++ src/main/java/fr/ghiloufi/interpreter/Interpreter.java
@@ -7,21 +7,50 @@ public class Interpreter {
 
 	private Pattern numericPattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
-	public String evaluate(List<String> instructions) {
-		if (isNumeric(instructions.get(0)))
-			return instructions.get(0);
-		if(isString(instructions.get(0))) {
-			return instructions.get(0).replace("\"", "");
+	public String evaluate(Object instruction) {
+		if (isNumeric(instruction)) {
+			return instruction.toString();
 		}
-		return null;
+		if (isString(instruction)) {
+			return instruction.toString().replace("\"", "");
+		}
+		if (isList(instruction)) {
+			@SuppressWarnings("unchecked")
+			List<Object> instructions = (List<Object>) instruction;
+			if (instructions.get(0).equals("+")) {
+				String operand_1 = evaluate(instructions.get(1));
+				String operand_2 = evaluate(instructions.get(2));
+				return Integer.toString(Integer.parseInt(operand_1) + Integer.parseInt(operand_2));
+			}
+			if (instructions.get(0).equals("-")) {
+				String operand_1 = evaluate(instructions.get(1));
+				String operand_2 = evaluate(instructions.get(2));
+				return Integer.toString(Integer.parseInt(operand_1) - Integer.parseInt(operand_2));
+			}
+			if (instructions.get(0).equals("*")) {
+				String operand_1 = evaluate(instructions.get(1));
+				String operand_2 = evaluate(instructions.get(2));
+				return Integer.toString(Integer.parseInt(operand_1) * Integer.parseInt(operand_2));
+			}
+			if (instructions.get(0).equals("/")) {
+				String operand_1 = evaluate(instructions.get(1));
+				String operand_2 = evaluate(instructions.get(2));
+				return Integer.toString(Integer.parseInt(operand_1) / Integer.parseInt(operand_2));
+			}
+		}
+		throw new RuntimeException(String.format("Unknown instruction \n %S", instruction.toString()));
 	}
 
-	private boolean isString(String string) {
-		return string.startsWith("\"") && string.endsWith("\"");
+	private boolean isList(Object instruction) {
+		return instruction instanceof List;
 	}
 
-	private boolean isNumeric(String string) {
-		return numericPattern.matcher(string).matches();
+	private boolean isString(Object instruction) {
+		return instruction.toString().startsWith("\"") && instruction.toString().endsWith("\"");
+	}
+
+	private boolean isNumeric(Object instruction) {
+		return numericPattern.matcher(instruction.toString()).matches();
 	}
 
 }
--- src/test/java/fr/ghiloufi/interpreter/InterpreterTest.java
+++ src/test/java/fr/ghiloufi/interpreter/InterpreterTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class InterpreterTest {
-	
+
 	private Interpreter interpreter;
 
 	@Before
@@ -19,14 +19,48 @@ public class InterpreterTest {
 
 	@Test
 	public void should_return_number() {
-		List<String> instructions = List.of("1");
-		assertEquals("1", interpreter.evaluate(instructions));
+		assertEquals("1", interpreter.evaluate("1"));
 	}
-	
+
 	@Test
 	public void should_return_string() {
-		List<String> instructions = List.of("\"Hello World !\"");
-		assertEquals("Hello World !", interpreter.evaluate(instructions));
+		assertEquals("Hello World !", interpreter.evaluate("\"Hello World !\""));
+	}
+
+	@Test
+	public void should_return_the_sum() {
+		List<Object> instructions_1 = List.of("+", "1", "2");
+		List<Object> instructions_2 = List.of("+", List.of("+", "2", "3"), "5");
+		assertEquals("3", interpreter.evaluate(instructions_1));
+		assertEquals("10", interpreter.evaluate(instructions_2));
+	}
+
+	@Test
+	public void should_return_the_substraction() {
+		List<Object> instructions_1 = List.of("-", "1", "2");
+		List<Object> instructions_2 = List.of("-", List.of("-", "2", "3"), "5");
+		assertEquals("-1", interpreter.evaluate(instructions_1));
+		assertEquals("-6", interpreter.evaluate(instructions_2));
+	}
+
+	@Test
+	public void should_return_the_power() {
+		List<Object> instructions_1 = List.of("*", "1", "2");
+		List<Object> instructions_2 = List.of("*", List.of("*", "2", "3"), "5");
+		assertEquals("2", interpreter.evaluate(instructions_1));
+		assertEquals("30", interpreter.evaluate(instructions_2));
+	}
+
+	@Test
+	public void should_return_the_division() {
+		List<Object> instructions_1 = List.of("/", "1", "2");
+		List<Object> instructions_2 = List.of("/", List.of("/", "2", "3"), "5");
+		List<Object> instructions_3 = List.of("/", "6", "2");
+		List<Object> instructions_4 = List.of("/", List.of("/", "6", "3"), "2");
+		assertEquals("0", interpreter.evaluate(instructions_1));
+		assertEquals("0", interpreter.evaluate(instructions_2));
+		assertEquals("3", interpreter.evaluate(instructions_3));
+		assertEquals("1", interpreter.evaluate(instructions_4));
 	}
 
 }
